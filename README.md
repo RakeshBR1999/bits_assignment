8e3d26b52fc67448cc937a8e7bd059118839d936
Rakesh B R <rakeshbr@Rakeshs-MacBook-Pro.local>
2022-12-23 09:45:20 +0530

    added solved assignment for the first sem mtech program

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# bits_assignment
+# Bits Data science and Engineering assignment-1
+
+## 1. Problem Statement
+
+Consider a tree with input data value given as a string for each node. Follow the constraints while taking the input (no white space in between the character) , it can be with any of the combination i.e. special character/alphabets/numeric value/all in one word (example: 2345/implement/the123&/789abcde/2@entry/@$!)...
+Construct a BST by comparing each input word by following the given ordered rules i.e., high priority given to Special character(! (least) @ # $ ^(highest)) only use these 5 special characters in the same order of priority as mentioned, next priority to numeric values ( 0 (least) to 9 (highest) ) and last priority to alphabets (small letters (least), capital letters (highest)). If the same/duplicate word is given as input then add a counter value of that node and increment it, and arrange rest of the tree nodes in the same fashion based on the rules (i.e., each character of the word to be compared and need to arrange them accordingly. example: 2345 and 2@entry as first character is same in both words then compare second character Which is 3 and @ from each word. Similarly, arrogance and arrange as the first 3 characters were the same in both the string, so compare next character o and a of both words) and place them in the binary search tree structure...
+For example, given
+1. [ 80186356719, $we456try, Arrogance, 9type21#jr, arrogance, !9n6lq0@p74jxaal, Imple1983^4#@ ]
+
+### Let us use the above constraints to answer the below questions:
+
+1. Figure out how many characters in each input value of all the nodes in the tree?
+2. Identify how many nodes in the tree are with the same string count and mention those words under the relevant count list.
+3. List out all the given input words based on the order of priority as mentioned in the problem statement from highest to least/least to highest.
+4. Find out the largest string count and the duplicate string after comparing all the nodes in the tree and those nodes position/level in the binary search tree.
+5. Specify all the node input values in the preorder traversal of BST.
+
+### Requirements:
+
+1. Read the input from a file (inputPS9.txt), where each Node should be with input given as string.
+2. You will output your answers to a file (outputPS9.txt) for each line.
+3. Perform an analysis for the features above and give the running time in terms of input size: n.
+
+
+### Design Document for Binary Search Tree Construction and Traversal
+
+#### Introduction
+The purpose of this design document is to outline the steps for constructing a binary search tree (BST) and performing various traversals on the tree. The BST will be constructed using a list of strings and a priority order, where the priority of each string is determined by the first character in the string. The traversals will include pre-order traversal, which prints the values of the nodes in order from smallest to largest, and finding the number of nodes in the tree with the same string count, the largest string count and the duplicate strings, and the level or height of a node in the tree.
+
+#### Data Structures
+    Node class: This class represents a single node in the BST. It has the following attributes:
+
+    value: The value of the node, which is a string.
+    priority: The priority of the node, which is determined by the first character in the value.
+    left: A reference to the left child of the node.
+    right: A reference to the right child of the node.
+
+#### BinaryTree class: This class represents the BST and has the following methods:
+
+    init: This method initializes the root node of the BST.
+    insert: This method inserts a new node into the BST. It takes a value and a priority as input and creates a new node with these values. It then traverses the tree to find the correct position for the new node based on the priority.
+    in_order_traversal: This method performs an pre-order traversal of the BST, printing the values of the nodes in order from smallest to largest.
+    count_nodes: This method counts the number of nodes in the BST with the same string count and returns a dictionary with the string count as the key and a list of strings with that count as the value.
+    find_largest_string_count: This method finds the largest string count in the BST and returns the count and a list of strings with that count.
+    find_duplicates: This method finds the duplicate strings in the BST and returns a list of these strings.
+    find_level: This method finds the level or height of a node in the BST and returns the level.
+
+#### Algorithms
+
+Insertion: To insert a new node into the BST, the following steps are performed:
+
+Create a new node with the given value and priority.
+    If the root node is None, set the root node to the new node.
+    Otherwise, set the current node to the root node.
+    While True:
+        If the new node's priority is greater than the current node's priority:
+        If the current node's right child is None, set the right child to the new node and break the loop.
+        Otherwise, set the current node to the right child.
+        Otherwise:
+        If the current node's left child is None, set the left child to the new node and break the loop.
+        Otherwise, set the current node to the left child.
+
+#### Pre-order traversal: 
+    To perform an Pre-order traversal of the BST, the following steps are performed:
+
+    If the current node is None, return.
+    Recursively traverse the left subtree.
+    Print the value of the current node.
+    Recursively traverse the right subtree.
+
+## Run time analysis
+
+The time complexity of constructing the binary search tree is O(n*l),
+ where n is the number of strings and l is the average length of the strings. 
+ This is because each string needs to be inserted into the tree, 
+ which requires traversing the tree to find the correct position for the new node. 
+ The time complexity for each insertion is O(l), 
+ because the priority of each string is determined by the first character, which has a maximum length of 1.
+
+The time complexity of the pre-order traversal is O(n), because it visits each node in the tree exactly once.
+
+The time complexity of finding the number of nodes in the tree with the same string count is O(n), 
+because it visits each node in the tree exactly once.
+
+The time complexity of finding the largest string count and the duplicate strings is O(n), 
+because it visits each node in the tree exactly once.
+
+The time complexity of finding the level or height of a node in the tree is O(l), 
+because it requires traversing the tree to find the node.
+
+Overall, the time complexity of the above operations is O(nl + n + n + n + l) = O(nl), 
+which is linear in the number of strings and the average length of the strings.
+
+It is important to note that these time complexities are in terms of the asymptotic notation, 
+which means that they represent the worst-case time complexity. 
+In practice, the actual time required to perform these operations may be significantly lower,
+depending on the specific input data and the implementation of the algorithms.
---
+++ bst.py
@@ -0,0 +1,200 @@
+class Node:
+    def __init__(self, value, priority):
+        self.value = value
+        self.priority = priority
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+#construction a tree
+    def insert(self, value, priority):
+        new_node = Node(value, priority)
+        if self.root is None:
+            self.root = new_node
+        else:
+            current_node = self.root
+            while True:
+                if new_node.priority > current_node.priority:
+                    # insert to the right
+                    if current_node.right is None:
+                        current_node.right = new_node
+                        break
+                    else:
+                        current_node = current_node.right
+                else:
+                    # insert to the left
+                    if current_node.left is None:
+                        current_node.left = new_node
+                        break
+                    else:
+                        current_node = current_node.left
+# pre-order traversal
+    def pre_order_traversal(self, node):
+        if node is not None:
+            print(node.value)
+            self.pre_order_traversal(node.left)
+            self.pre_order_traversal(node.right)
+            
+#function counting characters in each input value of all the nodes in the tree
+dupwords = [ ]
+def charcount(root):
+    if root is None:
+        return None
+    
+    # print(root.value,end="\n")
+    dupwords.append(root.value)
+    print(root.value+"--->has "+str(len(root.value))+" Characters",end="\n")
+    charcount(root.left)
+    charcount(root.right)
+
+#function for Listing all the given input words based on the order of priority from highest to least
+from functools import cmp_to_key
+def sort_strings(words, order):
+  # Create a mapping from alphabet to its position in the order
+  alphabet_map = {c: i for i, c in enumerate(order)}
+  
+  # Define a custom comparator function
+  def compare(word1, word2):
+    # Iterate through the characters in the two words
+    for c1, c2 in zip(word1, word2):
+      # If the characters have different positions in the order, return the result of the comparison
+      if alphabet_map[c1] != alphabet_map[c2]:
+        return alphabet_map[c1] - alphabet_map[c2]
+    # If the words have the same characters, return the result based on the length of the words
+    return len(word1) - len(word2)
+  
+  # Sort the array of strings using the custom comparator function
+  sorted_words = sorted(words, key=cmp_to_key(compare))
+  
+  return sorted_words
+
+#function for counting the nodes in the tree with the same count
+counts = {}
+def count_nodes(node):
+    if node is None:
+        return
+
+    # add the node to the counts dictionary
+    if len(node.value) not in counts:
+        counts[len(node.value)] = [node.value]
+    else:
+        counts[len(node.value)].append(node.value)
+
+    # recursively count the nodes in the left and right subtrees
+    count_nodes(node.left)
+    count_nodes(node.right)
+
+#function for finding the level
+def find_level(node, value, level):
+    if node is None:
+        return -1
+
+    if node.value == value:
+        return level
+
+    # search the left and right subtrees
+    left_level = find_level(node.left, value, level+1)
+    right_level = find_level(node.right, value, level+1)
+
+    # return the level if found, or -1 if not found
+    if left_level != -1:
+        return left_level
+    else:
+        return right_level
+
+#function for finding the duplicates
+def find_duplicates(node):
+    if node is None:
+        return []
+
+    # find the duplicates in the left and right subtrees
+    left_duplicates = find_duplicates(node.left)
+    right_duplicates = find_duplicates(node.right)
+
+    # check for duplicates in the current node
+    current_duplicates = []
+    if node.left is not None and node.left.value == node.value:
+        current_duplicates.append(node.left.value)
+    if node.right is not None and node.right.value == node.value:
+        current_duplicates.append(node.right.value)
+
+    # return the list of duplicates
+    return left_duplicates + right_duplicates + current_duplicates
+
+
+#create the priority order
+order = "^$#@!9876543210ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba"
+ss =''
+for i in order:
+  ss = i + ss
+
+
+#initialize the binary tree
+bt = BinaryTree()
+
+#reading the input from file
+import os
+# file = open(os.path.join(os.path.curdir, 'input.txt'),'r')
+strings = []
+file = open("inputPS9.txt",'r')
+lines = file.readlines()
+for i in lines:
+    strings.append(i.strip())
+
+#inserting values in to binary tree based on priority
+for s in strings:
+    priority = ss.index(s[0])
+    # insert the values into the binary tree
+    bt.insert(s, priority)
+
+print("1. Figure out how many characters in each input value of all the nodes in the tree?",end="\n\n")
+#counting characters in each input value of all the nodes in the tree
+charcount(bt.root)
+print()
+#printing total numbers of nodes in BST
+print("These are the total "+str(len(dupwords))+" nodes in the tree with character count of each node as above",end="\n\n")
+
+print("2. Identify how many nodes in the tree are with the same string count and mention those words under the relevant count list.",end="\n")
+#counting the nodes in the tree are with the same string count
+count_nodes(bt.root)
+
+print()
+for count,words in counts.items():
+    if len(words)>=2:
+        # print("\n")
+        print("There are "+str(len(words))+" nodes with same string count as "+str(count),end="\n")
+        print(str(words) + "--->"+ str(count),end="\n\n")
+
+print("3. List out all the given input words based on the order of priority as mentioned in the problem statement from highest to least/least to highest.",end="\n\n")
+#Listing all the given input words based on the order of priority from highest to least
+print("List of input word based on the order of Priority(higest to least)",end="\n\n")
+sorted_words = sort_strings(strings, order)
+for i in sorted_words:
+  print(i,end="\n")
+print("\n")
+
+#counting character and appending the counts
+l = [ ]
+for i in dupwords:
+  l.append(len(i))
+
+print("4. Find out the largest string count and the duplicate string after comparing all the nodes in the tree and those nodes position/level in the binary search tree.",end="\n\n")
+#Finding the largest string count and level in the binary search tree.
+for j in range(len(strings)):
+  if len(strings[j])==max(l):
+    level = find_level(bt.root, i, 0)
+    print("Largest string in all the nodes is with "+str(max(l))+ " as string count and it is at level "+str(level)+" in the BST..!",end="\n\n")
+
+#Finding the duplicate string and level in the binary search tree.
+duplicates = find_duplicates(bt.root)
+print("Duplicate input string in all the nodes is/are ",end="\n\n")
+for s in duplicates:
+    level = find_level(bt.root, s, 0)
+    print(s +" node repeated for "+str(dupwords.count(s))+ " times is at level ", str(level) +" in the BST..!",end="\n")
+print("\n")
+print("5. Specify all the node input values in the preorder traversal of BST.",end="\n\n")
+# printing the values in preorder
+print("Preorder traversal of the BST is",end="\n\n")
+bt.pre_order_traversal(bt.root)
